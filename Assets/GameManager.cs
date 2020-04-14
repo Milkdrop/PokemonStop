78908c86075fb1616c411dbb55f50aad1837e874
Milkdrop <milkdrop2000@protonmail.com>
2020-04-15 01:08:23 +0300

    First commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+    private int i = 0;
+    public Text uiText;
+
+    void Start() {
+        InvokeRepeating ("PingServer", 0, 1);
+    }
+
+    void PingServer () {
+        i++;
+        uiText.text = i + "";
+    }
+}
